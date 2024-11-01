82f9eb9b356bdc5c87a09e2eb4d22a3207a8fa28
Ezra <moug3r@gmail.com>
2024-11-01 15:32:33 +0200

    mobile support

---
+++ src/scripts.js
@@ -0,0 +1,7 @@
+// Toggle burger menu
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+  nav.classList.toggle('nav-active');
+});
